66620ce0f7993bf7d0176084b88faa9cb1b74e10
Alan Patrick <alan.patrick.fv@gmail.com>
2022-06-09 11:05:53 -0300

    Set basic layout idea

--- src/App.js
+++ src/App.js
@@ -6,6 +6,41 @@ export default class App extends Component {
   }
 
   render() {
-    return <h1>Hello, mortal</h1>;
+    return (
+      <div id="app">
+        <h2>Informações Gerais</h2>
+        <hr />
+        <form className="input-field">
+          <input type="text" id="basic-name" placeholder="Nome" />
+          <input type="text" id="basic-address" placeholder="Endereço" />
+          <input type="text" id="basic-email" placeholder="Email" />
+          <input type="text" id="basic-telefone" placeholder="Telefone" />
+        </form>
+        <h2>Formações Acadêmicas</h2>
+        <hr />
+        <form className="input-field">
+          <input type="text" id="course1-name" placeholder="Curso" />
+          <input type="text" id="course1-instution" placeholder="Instituição" />
+          <input type="text" id="course1-start" placeholder="Data de Início" />
+          <input
+            type="text"
+            id="course1-finish"
+            placeholder="Data de Término"
+          />
+        </form>
+        <h2>Experiências Profissionais</h2>
+        <hr />
+        <form className="input-field">
+          <input type="text" id="job1-company" placeholder="Empresa" />
+          <input type="text" id="job1-position" placeholder="Cargo" />
+          <input type="text" id="job1-start" placeholder="Data de Início" />
+          <input
+            type="text"
+            id="job1-finish"
+            placeholder="Data de Término"
+          />
+        </form>
+      </div>
+    );
   }
 }
