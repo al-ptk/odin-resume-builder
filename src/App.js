96d46e281cb9589a3dced82f45c27445d84f346b
Alan Patrick <alan.patrick.fv@gmail.com>
2022-06-09 11:33:19 -0300

    Complete basic layout idea

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ export default class App extends Component {
   render() {
     return (
       <div id="app">
-        <h2>Informações Gerais</h2>
+        <header className='category-header'>
+          <h2>Informações Gerais</h2>
+        </header>
         <hr />
         <form className="input-field">
           <input type="text" id="basic-name" placeholder="Nome" />
@@ -16,29 +18,35 @@ export default class App extends Component {
           <input type="text" id="basic-email" placeholder="Email" />
           <input type="text" id="basic-telefone" placeholder="Telefone" />
         </form>
-        <h2>Formações Acadêmicas</h2>
+        <header className='category-header'>
+          <h2>Formações Acadêmicas</h2>
+          <button>+</button>
+        </header>
         <hr />
         <form className="input-field">
           <input type="text" id="course1-name" placeholder="Curso" />
           <input type="text" id="course1-instution" placeholder="Instituição" />
-          <input type="text" id="course1-start" placeholder="Data de Início" />
+          <input type="date" id="course1-start" placeholder="Data de Início" />
+          <input type="checkbox" id="course1-isFinished" />
+          <label htmlFor="course1-isFinished">Em progresso?</label>
           <input
-            type="text"
+            type="date"
             id="course1-finish"
             placeholder="Data de Término"
           />
         </form>
-        <h2>Experiências Profissionais</h2>
+        <header className='category-header'>
+          <h2>Experiências Profissionais</h2>
+          <button>+</button>
+        </header>
         <hr />
         <form className="input-field">
           <input type="text" id="job1-company" placeholder="Empresa" />
           <input type="text" id="job1-position" placeholder="Cargo" />
-          <input type="text" id="job1-start" placeholder="Data de Início" />
-          <input
-            type="text"
-            id="job1-finish"
-            placeholder="Data de Término"
-          />
+          <input type="date" id="job1-start" placeholder="Data de Início" />
+          <input type="checkbox" id="job1-isFinished" />
+          <label htmlFor="job1-isFinished">Em progresso?</label>
+          <input type="date" id="job1-finish" placeholder="Data de Término" />
         </form>
       </div>
     );
